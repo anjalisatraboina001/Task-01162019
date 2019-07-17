34231b590315a4d3b48e459e69f88dd7dd62a41a
anjalisatraboina001 <26076934+anjalisatraboina001@users.noreply.github.com>
2019-07-16 19:34:10 -0600

    Update MovieWebApplication.java

--- webflux-thymeleaf/src/main/java/com/anjali/reactive/MovieWebApplication.java
+++ webflux-thymeleaf/src/main/java/com/anjali/reactive/MovieWebApplication.java
@@ -1,4 +1,4 @@
-package com.mkyong.reactive;
+package com.anjali.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
